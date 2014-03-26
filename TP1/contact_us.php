4d967e1abdd044409ce8646004cdaf2ea9c29a0a
MaxChaza <maxime.chazalviel@hotmail.fr>
2014-03-27 00:15:39 +0100

    n

--- TP1/contact_us.php
+++ TP1/contact_us.php
@@ -8,7 +8,7 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
-         <link rel="stylesheet" type="text/css" href="./css/style.css" />
+         <link rel="stylesheet" type="text/css" href="./css/styleCss.css" />
        
         <?php include('./include/counter.php');?>
         <script language="javascript" type="text/javascript">
@@ -23,28 +23,29 @@ and open the template in the editor.
         </div>
         <div id="formulaire"> 
             
-            <form name="myForm" action="contact_us.php" onsubmit="return checkParam()" method="post">
+            <form name="myForm" action="sendMail.php" onsubmit="return checkParam()" method="post">
                 <table>  
                     <tr>
                         <td id="colorPrenom">* Entrez prenom : </td>
-                        <td><input type="text" name="prenom"/></td>
+                        <td><input type="text" id="prenom" name="prenom"/></td>
                     </tr>
                     <tr>
                         <td id="colorNom">* Entrez nom :</td>
-                        <td><input type="text" name="nom"/></td>
+                        <td><input type="text" id="nom" name="nom"/></td>
                     </tr>
                     <tr>
                         <td id="colorTelephone">* Entrez telephone :</td> 
-                        <td><input type="text" name="telephone"/></td>
+                        <td><input type="text" id="telephone" name="telephone"/></td>
                         <td><div id="formatTelephone"></div></td>
                     </tr>
                     <tr>
                         <td id="colorMail">* Entrez mail :</td> 
-                        <td><input type="text" name="mail"/></td>
+                        <td><input type="text" id="mail" name="mail"/></td>
+                        <td><div id="formatMail"></div></td>
                     </tr>
                     <tr>   
                         <td>  Entrez texte : </td>
-                        <td><textarea name="textarea" rows=4 cols=40></textarea></td>
+                        <td><textarea name="message" rows=4 cols=40></textarea></td>
                     </tr>
                     <tr>
                         <td><input type="submit" value="go" /></td>
